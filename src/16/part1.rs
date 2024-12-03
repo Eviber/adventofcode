790b971e1a59fd470b1fc3175f04971c56a66f8e
Youva GAUDÉ <ygaude@solent.fr>
2024-12-03 22:54:32 +0100

    WIP day16 - basic parsing

--- src/16/part1.rs
+++ src/16/part1.rs
@@ -1,4 +1,42 @@
+// .|...\....
+// |.-.\.....
+// .....|-...
+// ........|.
+// ..........
+// .........\
+// ..../.\\..
+// .-.-/..|..
+// .|....-|.\
+// ..//.|....
 pub fn solve(input: &str) -> usize {
-    let _ = input;
-    0
+    let grid: Vec<Vec<_>> = input
+        .split_whitespace()
+        .map(|s| s.chars().map(Cell::from).collect())
+        .collect();
+    let energized = vec![vec![false; grid[0].len()]; grid.len()];
+
+    todo!()
+}
+
+use Cell::*;
+
+enum Cell {
+    Empty,
+    MirrorDescending,
+    MirrorAscending,
+    HorizSplitter,
+    VertiSplitter,
+}
+
+impl From<char> for Cell {
+    fn from(c: char) -> Self {
+        match c {
+            '.' => Empty,
+            '\\' => MirrorDescending,
+            '/' => MirrorAscending,
+            '-' => HorizSplitter,
+            '|' => VertiSplitter,
+            _ => panic!("invalid character"),
+        }
+    }
 }
