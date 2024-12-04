3da8db7a8fa4ecd43e173d5a986af23e5a6fcf38
Youva GAUDÉ <ygaude@solent.fr>
2024-12-04 22:59:00 +0100

    Day 4 - Part 1 done

--- src/2024/04/part1.rs
+++ src/2024/04/part1.rs
@@ -7,15 +7,11 @@ pub fn solve(input: &str) -> usize {
         .split_whitespace()
         .map(|s| s.chars().collect())
         .collect();
-    println!("h {}", input.matches("XMAS").count() + input.matches("SAMX").count());
-    println!("v {}", vert_matches(&grid, XMAS));
-    println!("\\ {}", diag_matches(&grid, XMAS));
-    println!("/ {}", diag2_matches(&grid, XMAS));
     input.matches("XMAS").count()
-    + input.matches("SAMX").count()
-    + vert_matches(&grid, XMAS)
-    + diag_matches(&grid, XMAS)
-    + diag2_matches(&grid, XMAS)
+        + input.matches("SAMX").count()
+        + vert_matches(&grid, XMAS)
+        + diag_matches(&grid, XMAS)
+        + diag2_matches(&grid, XMAS)
 }
 
 fn vert_matches(grid: &[Vec<char>], arg: &[char]) -> usize {
@@ -53,8 +49,8 @@ fn diag2_matches(grid: &[Vec<char>], arg: &[char]) -> usize {
     type Word = (char, char, char, char);
     let word = (arg[0], arg[1], arg[2], arg[3]);
     let back = (arg[3], arg[2], arg[1], arg[0]);
-    let x = (0..grid[0].len()).chain(repeat(grid[0].len()).take(grid.len() - 1));
-    let y = (repeat(0).take(grid[0].len())).chain(1..grid.len());
+    let x = (0..grid[0].len()).chain(repeat(grid[0].len()).take(grid.len()));
+    let y = (repeat(0).take(grid[0].len())).chain(0..grid.len());
     x.zip(y)
         .map(|(x, y)| zip((0..x).rev(), y..grid.len()))
         .map(|i| i.map(|(x, y)| grid[y][x]))
