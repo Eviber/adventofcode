83f9d9ac1ea4b6750eb6220b82a382454b7b75df
Youva GAUDÉ <ygaude@solent.fr>
2024-12-05 00:09:57 +0100

    Day 4 - Part 2 done

--- src/2024/04/part2.rs
+++ src/2024/04/part2.rs
@@ -1,3 +1,22 @@
+use std::iter::repeat;
+
 pub fn solve(input: &str) -> usize {
-    input.len()
+    let grid: Vec<Vec<char>> = input
+        .split_whitespace()
+        .map(|s| s.chars().collect())
+        .collect();
+    (1..(grid.len() - 1))
+        .flat_map(|y| {
+            (1..(grid.len() - 1))
+                .zip(repeat(y))
+                .filter(|&(x, y)| grid[y][x] == 'A')
+                .filter(|(x, y)| valid_pair(grid[y - 1][x - 1], grid[y + 1][x + 1]))
+                .filter(|(x, y)| valid_pair(grid[y - 1][x + 1], grid[y + 1][x - 1]))
+        })
+        .count()
+}
+
+#[inline]
+fn valid_pair(a: char, b: char) -> bool {
+    (a == 'M' && b == 'S') || (a == 'S' && b == 'M')
 }
