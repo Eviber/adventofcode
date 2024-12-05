1b64e3512f1c91bc9927dc31068aa742510c5d09
Youva GAUDÉ <ygaude@solent.fr>
2024-12-05 08:20:41 +0100

    Day 5 - Part 1 done

---
+++ src/2024/05/part1.rs
@@ -0,0 +1,31 @@
+use std::collections::HashMap;
+
+pub fn solve(input: &str) -> usize {
+    let (rules_str, updates) = input.split_once("\n\n").expect("correct format");
+    let mut rules: HashMap<usize, Vec<usize>> = HashMap::new();
+    for s in rules_str.split_whitespace() {
+        let (s1, s2) = s.split_once('|').unwrap();
+        let page_1: usize = s1.parse().unwrap();
+        let page_2: usize = s2.parse().unwrap();
+        rules
+            .entry(page_1)
+            .and_modify(|v| v.push(page_2))
+            .or_insert(vec![page_2]);
+    }
+    let updates: Vec<Vec<usize>> = updates
+        .split_whitespace()
+        .map(|s| s.split(',').map(|s| s.parse().unwrap()).collect())
+        .collect();
+    updates
+        .into_iter()
+        .filter(|update| {
+            update.iter().enumerate().all(|(i, page)| {
+                update
+                    .iter()
+                    .take(i)
+                    .all(|n| rules.get(page).map(|v| !v.contains(n)).unwrap_or(true))
+            })
+        })
+        .map(|update| update[update.len() / 2])
+        .sum()
+}
