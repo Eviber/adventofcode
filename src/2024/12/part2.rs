911bf0e7fdc3872b04a8a11a9f28a1b9b164192b
Youva GAUDÉ <ygaude@solent.fr>
2024-12-12 09:13:10 +0100

    Remove visited loop

--- src/2024/12/part2.rs
+++ src/2024/12/part2.rs
@@ -22,46 +22,40 @@ fn map_region(
     garden: &[Vec<char>],
     visited: &mut [Vec<bool>],
 ) -> (usize, usize) {
-    let mut in_region = vec![vec![false; garden[0].len()]; garden.len()];
+    let mut region = vec![vec![false; garden[0].len()]; garden.len()];
     let mut file = VecDeque::new();
     file.push_back((x, y));
     let plant = garden[y][x];
     let mut area = 0;
     while let Some((x, y)) = file.pop_front() {
-        if y >= garden.len() || x >= garden[y].len() || in_region[y][x] || garden[y][x] != plant {
+        if y >= garden.len() || x >= garden[y].len() || visited[y][x] || garden[y][x] != plant {
             continue;
         }
-        in_region[y][x] = true;
+        region[y][x] = true;
+        visited[y][x] = true;
         area += 1;
         file.push_back((x.wrapping_sub(1), y));
         file.push_back((x, y.wrapping_sub(1)));
         file.push_back((x + 1, y));
         file.push_back((x, y + 1));
     }
-    let sides = calc_sides(&in_region);
-    for (y, line) in in_region.into_iter().enumerate() {
-        for (x, b) in line.into_iter().enumerate() {
-            if b {
-                visited[y][x] = true;
-            }
-        }
-    }
+    let sides = calc_sides(&region);
     (area, sides)
 }
 
-fn calc_sides(in_region: &[Vec<bool>]) -> usize {
+fn calc_sides(region: &[Vec<bool>]) -> usize {
     let mut sides = 0;
-    for y in 0..in_region.len() {
+    for y in 0..region.len() {
         let mut up_border = false;
         let mut down_border = false;
-        for x in 0..in_region[0].len() {
-            if (in_region[y][x] && (y == 0 || !in_region[y - 1][x])) != up_border {
+        for x in 0..region[0].len() {
+            if (region[y][x] && (y == 0 || !region[y - 1][x])) != up_border {
                 up_border = !up_border;
                 if up_border {
                     sides += 1;
                 }
             }
-            if (in_region[y][x] && (y >= in_region.len() - 1 || !in_region[y + 1][x]))
+            if (region[y][x] && (y >= region.len() - 1 || !region[y + 1][x]))
                 != down_border
             {
                 down_border = !down_border;
@@ -71,10 +65,10 @@ fn calc_sides(in_region: &[Vec<bool>]) -> usize {
             }
         }
     }
-    for x in 0..in_region[0].len() {
+    for x in 0..region[0].len() {
         let mut left_border = false;
         let mut right_border = false;
-        for line in in_region {
+        for line in region {
             if (line[x] && (x == 0 || !line[x - 1])) != left_border {
                 left_border = !left_border;
                 if left_border {
